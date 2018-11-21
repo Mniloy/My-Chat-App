1acf6b4674b13c673f13290b089a4f42ba3a2789
Mniloy <54440@siswa.unimas.my>
2018-11-21 14:06:26 +0800

    Commited

---
+++ app/src/main/java/CodelabPreferences.java
@@ -0,0 +1,8 @@
+package com.example.a54440.messaging;
+
+public class CodelabPreferences {
+
+    public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
